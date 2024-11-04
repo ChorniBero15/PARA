d6a0a2daaf65b2d1447549d4b2cedb9a5273eb09
ChorniBero <toberi23@freeuni.edu.ge>
2024-11-04 18:15:24 +0400

    fib.s

--- seminars/luka/09-10/fib.s
+++ seminars/luka/09-10/fib.s
@@ -18,42 +18,41 @@
 #     return 0;
 # }
 
-main:
-    addi sp, sp, -4
-    
+li x10, 7
+addi sp, sp, -4
+sw x10, 0(sp)
+call fib
+li x10, 1
+mv x11, x20
+ecall
+li x10, 10
+ecall
+
+fib: 
+    lw x10, 0(sp)
+    addi x11, x0, 1
+    bgt x10, x11, else
+    mv x20, x10
+    ret
+
+else: 
     addi sp, sp, -8
-    li x10, 5
-    sw  x10, 4(sp)
-    sw ra, 0(sp)
-    
+    sw ra, 4(sp)
+    lw x10, 8(sp)
+    addi x10, x10, -1
+    sw x10, 0(sp)
     call fib
-    
-    lw ra, 0(sp)
+    addi sp, sp, -4
+    sw x20, 0(sp)
+    addi sp, sp, -4
+    lw x10, 8(sp)
+    addi x10, x10, -1
+    sw x10, 0(sp)
+    call fib
+    addi sp, sp, 4
+    lw x10, 0(sp)
+    add x20, x10, x20
     addi sp, sp, 8
-    sw a0, 0(sp)
-    
-    li x10, 1
-    lw x11, 0(sp)
-    ecall
-    
-    li x10, 10
-    ecall
-    
-    
-    fib:
-        addi sp, sp, -8
-        lw x10, 12(sp)
-        li x11, 1
-        BGT x10, x11, rec
-        mv a0, x10
-        j end
-        
-   rec:
-        
-    
-   end:
-       addi sp, sp, 8
-       ret
-   
-    
-   
+    lw ra, 0(sp)
+    addi sp, sp, 4
+    ret
