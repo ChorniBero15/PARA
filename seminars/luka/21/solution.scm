66e8cdc171fb3c9561b537ddeda2680aadcd4474
ChorniBero <toberi23@freeuni.edu.ge>
2025-01-06 23:17:07 +0400

    bolo

--- seminars/luka/21/solution.scm
+++ seminars/luka/21/solution.scm
@@ -1,47 +1,65 @@
-(define (digits n)
-
-)
-
-
-;; (define (mapfn x)
-;;   (expt x (length lst))
-;;   )
-
-(define (power n k)
-
+(define (digits n) 
+    (if (< n 10) (list n)
+        (append (digits (quotient n 10))
+                (list (modulo n 10)))
+    )
 )
 
 (define (list_power lst)
-)
+  (let ((n (length lst)))
+    (map (lambda (num) (expt num n)) lst)))
+
 
 (define (sum lst)
+    (apply + lst)
 )
 
 (define (narcisisstic? n)
+    (if (= n (sum (list_power (digits n)))) #t
+        #f 
+    )
 )
 
 (define (count n lst)
+    (cond 
+        ((null? lst) 0)
+        ((= n (car lst)) (+ 1 (count n (cdr lst))))
+        (else (count n (cdr lst)))
+    )
 )
 
-(define (count2 n lst)
-)
 
-(define (set lst)
+(define (set_helper lst lst0)
+    (cond
+        ((null? lst) lst0)
+        ((zero? (count (car lst) lst0)) 
+            (set_helper (cdr lst) (cons (car lst) lst0)))
+        (else (set_helper (cdr lst) lst0))
+    )
+) 
+
+(define (set lst) 
+    (set_helper lst '())
 )
 
 (define (frequency lst)
+	(map (lambda (numb) (list numb (count numb lst))) (set lst))
 )
+
 
 (define (multiplyone n k)
+	(if (zero? n) '()
+		(cons k (multiplyone (- n 1) k))
+	)
 )
 
-;; ((1 1 1) (2 2 2)) -> (append (1 1 1) (2 2 2))
 (define (multiply n lst)
-	(apply append (map (lambda (x) (multiplyone n x)) lst))
+    (apply append (map (lambda (numb) (multiplyone n numb)) lst))
 )
 
 
 (define (ksubset n lst)
+	
 )
 
 
